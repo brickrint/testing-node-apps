1bc76aea49e5564de35fe52168ff0a83365e0d7c
Rinat <75369047+brickrint@users.noreply.github.com>
2024-03-27 20:14:36 +0000

    auth.js jest-in-case

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,25 +1,24 @@
+import cases from 'jest-in-case';
 import { isPasswordAllowed } from '../auth';
 
-test('when valid password provided', () => {
-  const res = isPasswordAllowed('!aBc123');
+cases('when valid password provided: (pass)', (opts) => {
+  const res = isPasswordAllowed(opts.pass);
   expect(res).toBe(true);
+}, {
+  "!aBc123": { pass: '!aBc123' }
 });
 
-describe('when incorrect password provided', () => {
-  const incorrectPasswords = [
-    "a2c!",
-    "123456!",
-    "ABCdef!",
-    "abc123!",
-    "ABC123!",
-    "ABCdef123"
-  ];
-
-  test.each(incorrectPasswords)('when password is %s', (password) => {
-    const res = isPasswordAllowed(password);
-    expect(res).toBe(false);
-  });
-});
+cases('when invalid provided: (pass)', (opts) => {
+  const res = isPasswordAllowed(opts.pass);
+  expect(res).toBe(false);
+}, {
+  "a2c!": {pass: "a2c!"},
+  "123456!": {pass: "123456!"},
+  "ABCdef!": {pass: "ABCdef!"},
+  "abc123!": {pass: "abc123!"},
+  "ABC123!": {pass: "ABC123!"},
+  "ABCdef123": {pass: "ABCdef123"}
+})
 
 // 🐨 write tests for valid and invalid passwords
 // 💰 here are some you can use:
